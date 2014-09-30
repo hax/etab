2e4e534ecdec1c5b7dd04210572d5f208efc15d0
HE Shi-Jun <hax@heshijun.net>
2014-09-30 18:26:45 +0800

    add user script head

---
+++ src/github-head.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name       elastic tabstops view for github
+// @namespace  http://github.com/hax/
+// @version    0.1
+// @description  
+// @match      http://github.com/*
+// @copyright  2014, Hax
+// ==/UserScript==
