7f10b64ffc2e570162fa5fd237a7358e87fba171
HE Shi-Jun <hax@heshijun.net>
2014-10-09 19:52:08 +0800

    adjust

--- example/hanging-punc.js
+++ example/hanging-punc.js
@@ -1,13 +1,15 @@
 'use strict'
 
-/* @class */	function Point	(/* @param string */	name,
-		/* @param int */	x,
-		/* @param int */	y) {
+/* @class */
+function Point	(/* @param string */	name,
+	/* @param int */	x,
+	/* @param int */	y)	{
 
 		this._name	= name
 		this.x	= x
 		this.y	= y
-	}
-	/* @method string */	Point.prototype.getName	= function () {
-		return this._name
+		}
+/* @method string */
+Point.prototype.getName = function ()	{
+	return this._name
 	}
