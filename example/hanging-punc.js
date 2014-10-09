1e41de9c8bb1c9deaeddb1a429ae733e6e69fed7
HE Shi-Jun <hax@heshijun.net>
2014-10-09 20:40:40 +0800

    typo

--- example/hanging-punc.js
+++ example/hanging-punc.js
@@ -2,7 +2,7 @@
 
 function Point	(/* @param string	*/	name,	// Name of the point
 	/* @param int	*/	x,	// X coordinate on the map
-	/* @param int	*/	y,	// Y coordinate on the map
+	/* @param int	*/	y	// Y coordinate on the map
 	) {
 		this.name	=	String(name)
 		this.x	=	x|0
