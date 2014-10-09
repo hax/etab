d3d4e4153d79623986f36a3180bb675f9daaa8a4
HE Shi-Jun <hax@heshijun.net>
2014-10-09 19:54:13 +0800

    adjust

--- example/hanging-punc.js
+++ example/hanging-punc.js
@@ -3,13 +3,13 @@
 /* @class */
 function Point	(/* @param string */	name,
 	/* @param int */	x,
-	/* @param int */	y)	{
-
+	/* @param int */	y
+	) {
 		this._name	= name
 		this.x	= x
 		this.y	= y
-		}
-/* @method string */
-Point.prototype.getName = function ()	{
-	return this._name
 	}
+/* @method string */
+Point.prototype.getName = function () {
+	return this._name
+}
