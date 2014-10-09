0a904b184418a6c2a83bbc8fec52c4c1ee597a1c
HE Shi-Jun <hax@heshijun.net>
2014-10-09 17:01:56 +0800

    hang open puncs for github

--- src/github.js
+++ src/github.js
@@ -2,10 +2,17 @@ var etab = new ElasticTabstops({
 	styleRules: [
 		".blob-code { font-family: 'Input Serif Narrow', 'Georgia', serif; font-size: 1.167em; }",
 		".highlight .p { color: #bbb; font-weight: lighter; }",
+		".highlight span.tab-char + .open.p { position: absolute; transform: translateX(-100%); }",
 	]
 })
 
 function process() {
+	var openPuncs = /^["'([{“‘]+$/
+	var puncs = document.querySelectorAll('.highlight .p')
+	for (var i = 0; i < puncs.length; i++) {
+		if (openPuncs.test(puncs[i].textContent)) puncs[i].classList.add('open')
+	}
+	
 	etab.processLines(document.querySelectorAll('.js-file-line'))
 }
 
