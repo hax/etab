bafd95ba32743c65549f392295390196060c9090
HE Shi-Jun <hax@heshijun.net>
2014-10-13 17:13:40 +0800

    redist

---
+++ dist/github.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name	github-etab
+// @namespace	https://github.com/hax/etab
+// @version	0.6
+// @description	Support elastic tabstops for github.com
+// @match	https://github.com/*
+// @updateURL	https://raw.githubusercontent.com/hax/etab/master/dist/github.meta.js
+// @downloadURL	https://raw.githubusercontent.com/hax/etab/master/dist/github.user.js
+// @copyright	2014, Hax
+// ==/UserScript==
