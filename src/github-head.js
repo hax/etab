fa839c033e9add574d1c4868b82cc29e962ea04d
HE Shi-Jun <hax@heshijun.net>
2014-10-13 16:19:25 +0800

    update version and redist

--- src/github-head.js
+++ src/github-head.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name	elastic tabstops view for github
 // @namespace	https://github.com/hax/
-// @version	0.3
+// @version	0.4
 // @description	
 // @match	https://github.com/*
 // @copyright	2014, Hax
