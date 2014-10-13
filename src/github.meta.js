e937def2c7216ffcca788a96615011dd7236672c
HE Shi-Jun <hax@heshijun.net>
2014-10-13 16:35:59 +0800

    update meta

--- src/github.meta.js
+++ src/github.meta.js
@@ -1,8 +1,10 @@
 // ==UserScript==
-// @name	elastic tabstops view for github
-// @namespace	https://github.com/hax/
-// @version	0.4
-// @description	
+// @name	github-etab
+// @namespace	https://github.com/hax/etab
+// @version	0.5
+// @description	Support elastic tabstops for github.com
 // @match	https://github.com/*
+// @updateURL	https://github.com/hax/etab/raw/master/src/github.meta.js
+// @downloadURL	https://github.com/hax/etab/raw/master/dist/github.user.js
 // @copyright	2014, Hax
 // ==/UserScript==
