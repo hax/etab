29ee5c939e84555cf0d7367f1c4b59c48fdd21bd
HE Shi-Jun <hax@heshijun.net>
2014-12-24 21:06:28 +0800

    dist

--- dist/github.meta.js
+++ dist/github.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name	github-etab
 // @namespace	https://github.com/hax/etab
-// @version	0.6
+// @version	0.7
 // @description	Support elastic tabstops for github.com
 // @match	https://github.com/*
 // @updateURL	https://raw.githubusercontent.com/hax/etab/master/dist/github.meta.js
