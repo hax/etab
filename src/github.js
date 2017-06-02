c2ab9bc3398e1bea5007b42a0d73e815815942a0
HE Shi-Jun <hax@heshijun.net>
2017-06-02 15:35:47 +0800

    use github loader

--- src/github.js
+++ src/github.js
@@ -12,11 +12,13 @@ function process() {
 	for (var i = 0; i < puncs.length; i++) {
 		if (openPuncs.test(puncs[i].textContent)) puncs[i].classList.add('open')
 	}
-	
+
 	var blobs = document.querySelectorAll('.blob-wrapper')
 	for (var i = 0; i < blobs.length; i++) {
 		etab.processLines(blobs[i].querySelectorAll('.blob-code'))
 	}
 }
 
-$(document).ready(process).on('pjax:success', process)
+ghImport('jquery').then($ => {
+	$(document).ready(process).on('pjax:success', process)
+})
