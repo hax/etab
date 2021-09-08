58436ed057537bb3e7822e298d43100642ec299a
HE Shi-Jun <hax@heshijun.net>
2021-09-08 19:19:30 +0800

    align with current github site

--- src/github.js
+++ src/github.js
@@ -1,14 +1,14 @@
 var etab = new ElasticTabstops({
 	styleRules: [
 		".blob-code { font-family: 'Input Serif Narrow', 'Georgia', serif; font-size: 1.167em; }",
-		".highlight .p { color: #bbb; font-weight: lighter; }",
-		".highlight span.tab-char + .open.p { position: absolute; transform: translateX(-100%); }",
+		".highlight .pl-kos { color: #bbb; font-weight: lighter; }",
+		".highlight span.tab-char + .open.pl-kos { position: absolute; transform: translateX(-100%); }",
 	]
 })
 
 function process() {
 	var openPuncs = /^["'([{“‘]+$/
-	var puncs = document.querySelectorAll('.highlight .p')
+	var puncs = document.querySelectorAll('.highlight .pl-kos')
 	for (var i = 0; i < puncs.length; i++) {
 		if (openPuncs.test(puncs[i].textContent)) puncs[i].classList.add('open')
 	}
@@ -19,6 +19,7 @@ function process() {
 	}
 }
 
-ghImport('jquery').then(function($) {
-	$(document).ready(process).on('pjax:success', process)
-})
+window.addEventListener('show', process)
+// ghImport('jquery').then(function($) {
+// 	$(document).ready(process).on('pjax:success', process)
+// })
